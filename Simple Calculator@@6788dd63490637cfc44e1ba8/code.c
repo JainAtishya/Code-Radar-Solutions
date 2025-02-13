a277d91f21e21a3252a923b72af35472766aab7a
Atishya Jain <atishyajain62149@gmail.com>
2025-02-13 17:29:53 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,10 +3,9 @@
 int main(){
     int a,b;
     char operator;
-    scanf("%d %d %c", &a, &operator, &b);
+    scanf("%d %c %d ", &a, &operator, &b);
     if(operator == '+'){
-        int sum = a+b;
-        printf("%d", sum);
+        printf("%d", (a + b));
     }else if(operator == '-'){
         printf("%d", (a - b));
     }else if(operator == '*'){

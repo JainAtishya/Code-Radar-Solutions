efa6f61a2cbb89fbfca7d41175a3aeadcd065233
Atishya Jain <atishyajain62149@gmail.com>
2025-04-01 11:20:15 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,29 +1,73 @@
-// Your code here...
+// // Your code here...
+// #include <stdio.h>
+
+// int main(){
+//     int N;
+//     scanf("%d", &N);
+//     int arr[N];
+//     for(int i = 0; i < N; i++){
+//         scanf("%d", &arr[i]);
+//     }
+    
+//     for(int i = 1; i < N-1; i++){
+//         if(arr[0]> arr[1]){
+//         printf("%d", arr[0]);
+//         break;
+//     }
+//     if(arr[N-1]> arr[N-2]){
+//         printf("%d", arr[N-1]);
+//         break;
+//     }
+//         if((arr[i] > arr[i-1]) && (arr[i] > arr[i+1])){
+//             printf("%d", arr[i]);
+//             break;
+//         }else if(i == N-2){
+//             printf("-1");
+//         }
+//     }
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
-int main(){
+int main() {
     int N;
     scanf("%d", &N);
+    
+    if (N <= 0) {
+        printf("-1"); // Handling invalid input case
+        return 0;
+    }
+
     int arr[N];
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
     }
-    
-    for(int i = 1; i < N-1; i++){
-        if(arr[0]> arr[1]){
+
+    // Check first and last elements separately
+    if (N == 1) {
+        printf("%d", arr[0]); // Only one element, it's the peak
+        return 0;
+    }
+    if (arr[0] > arr[1]) {
         printf("%d", arr[0]);
-        break;
+        return 0;
     }
-    if(arr[N-1]> arr[N-2]){
+    if (arr[N-1] > arr[N-2]) {
         printf("%d", arr[N-1]);
-        break;
+        return 0;
     }
-        if((arr[i] > arr[i-1]) && (arr[i] > arr[i+1])){
+
+    // Check for peak elements in the middle
+    for (int i = 1; i < N - 1; i++) {
+        if ((arr[i] > arr[i-1]) && (arr[i] > arr[i+1])) {
             printf("%d", arr[i]);
-            break;
-        }else if(i == N-2){
-            printf("-1");
+            return 0;  // Print first peak found and exit
         }
     }
+
+    // If no peak is found
+    printf("-1");
     return 0;
 }
